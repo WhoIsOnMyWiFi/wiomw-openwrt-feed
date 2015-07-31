df29fd9378a2f95c7be924eeb44cf5c3a7aa2d2c
Charles Southerland <csoutherland@io3o.com>
2015-07-31 11:06:59 -0500

    Preparing the feed for github

--- luci-app-wiomw/files/wiomw-controller.lua
+++ luci-app-wiomw/files/wiomw-controller.lua
@@ -6,9 +6,10 @@ function index()
 	end
 
 	entry({"admin", "wiomw"}, firstchild(), _("Who Is On My WiFi"), 90).index=true
-	entry({"admin", "wiomw", "login"}, template("wiomw/login"), _("Who Is On My WiFi Login"), 10)
+--	entry({"admin", "wiomw", "login"}, template("wiomw/login"), _("Who Is On My WiFi Login"), 10)
+	entry({"admin", "wiomw", "basic"}, cbi("wiomw/basic"), _("WIOMW Basic Settings"), 15)
 --	entry({"admin", "wiomw", "advanced"}, cbi("wiomw/advanced"), _("WIOMW Advanced Settings"), 20)
 --	entry({"admin", "wiomw", "logtail"}, template("wiomw/logtail"), _("WIOMW Agent Log"), 30)
-	entry({"wiomw", "basic"}, cbi("wiomw/basic"), _("WIOMW Basic Settings"), 91).dependent=false
+--	entry({"wiomw", "basic"}, cbi("wiomw/basic"), _("WIOMW Basic Settings"), 91).dependent=false
 
 end
